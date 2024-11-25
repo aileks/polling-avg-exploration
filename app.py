933c3ad35bb5cb345cd7e8eb37eefb10c129c143
aileks <aeverly14@pm.me>
2024-11-25 18:06:16 -0500

    some styling updates

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ from shiny import reactive
 from shiny.express import ui, input
 from shinywidgets import render_plotly
 
-ui.page_opts(title="2024 Swing State Polling Data", fillable=True)
+ui.page_opts(
+    title="2024 Swing State Polling Data",
+    fillable=True,
+    page_navbar=True,
+)
 infile = Path(__file__).parent / "data/swing_polling_pivot_data.csv"
 polling_data = pd.read_csv(infile)
+
 
 with ui.sidebar():
     ui.input_select(
@@ -24,20 +29,29 @@ def dat():
     return selected_state_data
 
 
-with ui.layout_columns():
+with ui.div(class_="d-flex justify-content-center w-100"):
+    with ui.layout_columns():
 
-    @render_plotly
-    def plot1():
-        state_data = dat()
-        return px.bar(
-            state_data,
-            x="state",
-            y=["Harris", "Trump"],
-            barmode="group",
-            title=f"Polling Averages for {input.state()}",
-            labels={
-                "value": "Average Polling Percentage",
-                "variable": "Candidate",
-                "state": "State",
-            },
-        )
+        @render_plotly
+        def plot():
+            state_data = dat()
+            fig = px.bar(
+                state_data,
+                x="state",
+                y=["Harris", "Trump"],
+                barmode="group",
+                title=f"Polling Averages for {input.state()}",
+                labels={
+                    "value": "Average Polling Percentage",
+                    "variable": "Candidate",
+                    "state": "State",
+                },
+            )
+
+            fig.update_layout(
+                autosize=True,
+                width=1000,
+                height=800,
+                margin=dict(l=50, r=50, t=50, b=50),
+            )
+            return fig
